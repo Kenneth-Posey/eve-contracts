814c9656e0faf1210f260ab6a88523bb42a67833
Kenneth Posey <kenneth.posey@gmail.com>
2015-03-04 13:13:01 -0800

    remove leftover panel

--- evetool/EVETool/Panhandler/MainInterface.Designer.cs
+++ evetool/EVETool/Panhandler/MainInterface.Designer.cs
@@ -34,50 +34,37 @@ namespace Panhandler
         /// </summary>
         private void InitializeComponent()
         {
-            this.panel1 = new System.Windows.Forms.Panel();
-            
             this.calculator = new Panhandler.CalculatorTab.CalculatorContainer();
             this.market = new Panhandler.MarketTab.MarketContainer();
             this.users = new Panhandler.UserTab.UserContainer();
 
             this.tabContainer = new System.Windows.Forms.TabControl();
             this.calculatorTabPage = new System.Windows.Forms.TabPage();
-            this.memberTabPage = new System.Windows.Forms.TabPage();
+            this.userTabPage = new System.Windows.Forms.TabPage();
             this.marketTabPage = new System.Windows.Forms.TabPage();
-            this.panel1.SuspendLayout();
+
             this.tabContainer.SuspendLayout();
             this.calculatorTabPage.SuspendLayout();
+            this.marketTabPage.SuspendLayout();
+            this.userTabPage.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // panel1
-            // 
-            this.panel1.Controls.Add(this.calculator);
-            this.panel1.Location = new System.Drawing.Point(6, 6);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(430, 346);
-            this.panel1.TabIndex = 16;
-            // 
-            // calculator
-            // 
-            this.calculator.Location = new System.Drawing.Point(3, 3);
-            this.calculator.Name = "calculator";
-            this.calculator.Size = new System.Drawing.Size(434, 337);
-            this.calculator.TabIndex = 132;
+            
             // 
             // tabContainer
             // 
             this.tabContainer.Controls.Add(this.calculatorTabPage);
-            this.tabContainer.Controls.Add(this.memberTabPage);
+            this.tabContainer.Controls.Add(this.userTabPage);
             this.tabContainer.Controls.Add(this.marketTabPage);
             this.tabContainer.Location = new System.Drawing.Point(12, 12);
             this.tabContainer.Name = "tabContainer";
             this.tabContainer.SelectedIndex = 0;
             this.tabContainer.Size = new System.Drawing.Size(1161, 407);
             this.tabContainer.TabIndex = 17;
+
             // 
             // calculatorTabPage
             // 
-            this.calculatorTabPage.Controls.Add(this.panel1);
+            this.calculatorTabPage.Controls.Add(this.calculator);
             this.calculatorTabPage.Location = new System.Drawing.Point(4, 22);
             this.calculatorTabPage.Name = "calculatorTabPage";
             this.calculatorTabPage.Padding = new System.Windows.Forms.Padding(3);
@@ -85,17 +72,26 @@ namespace Panhandler
             this.calculatorTabPage.TabIndex = 0;
             this.calculatorTabPage.Text = "Calculator";
             this.calculatorTabPage.UseVisualStyleBackColor = true;
+
             // 
-            // memberTabPage
+            // calculator
             // 
-            this.memberTabPage.Controls.Add(this.users);
-            this.memberTabPage.Location = new System.Drawing.Point(4, 22);
-            this.memberTabPage.Name = "memberTabPage";
-            this.memberTabPage.Padding = new System.Windows.Forms.Padding(3);
-            this.memberTabPage.Size = new System.Drawing.Size(1153, 381);
-            this.memberTabPage.TabIndex = 1;
-            this.memberTabPage.Text = "Members";
-            this.memberTabPage.UseVisualStyleBackColor = true;
+            this.calculator.Location = new System.Drawing.Point(3, 3);
+            this.calculator.Name = "calculator";
+            this.calculator.Size = new System.Drawing.Size(434, 337);
+            this.calculator.TabIndex = 132;
+
+            // 
+            // userTabPage
+            // 
+            this.userTabPage.Controls.Add(this.users);
+            this.userTabPage.Location = new System.Drawing.Point(4, 22);
+            this.userTabPage.Name = "userTabPage";
+            this.userTabPage.Padding = new System.Windows.Forms.Padding(3);
+            this.userTabPage.Size = new System.Drawing.Size(1153, 381);
+            this.userTabPage.TabIndex = 1;
+            this.userTabPage.Text = "Members";
+            this.userTabPage.UseVisualStyleBackColor = true;
 
             //
             // users
@@ -134,19 +130,20 @@ namespace Panhandler
             this.Controls.Add(this.tabContainer);
             this.Name = "MainInterface";
             this.Text = "Custom Value Calculator";
-            this.panel1.ResumeLayout(false);
+
             this.tabContainer.ResumeLayout(false);
             this.calculatorTabPage.ResumeLayout(false);
+            this.marketTabPage.ResumeLayout(false);
+            this.userTabPage.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.TabControl tabContainer;
         private System.Windows.Forms.TabPage calculatorTabPage;
-        private System.Windows.Forms.TabPage memberTabPage;
+        private System.Windows.Forms.TabPage userTabPage;
         private System.Windows.Forms.TabPage marketTabPage;
 
         private CalculatorContainer calculator;
